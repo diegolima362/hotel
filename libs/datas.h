b5a894ab468bda67bfedc7f5d393c08f0ebfb6b6
diegolima362 <diegolima362@gmail.com>
2019-11-17 22:43:41 -0300

    after merge

---
+++ libs/datas.h
@@ -0,0 +1,12 @@
+//
+// Created by Diego on 03/09/2019.
+//
+
+#ifndef HOTEL_DATAS_H
+#define HOTEL_DATAS_H
+
+#endif //HOTEL_DATAS_H
+
+typedef struct tm DATE;
+
+DATE *ler_data();
---
+++ libs/extrafuncs.h
@@ -0,0 +1,33 @@
+//
+// Created by diego on 03/09/2019.
+//
+
+#ifndef HOTEL_EXTRA_FUNCS_H
+#define HOTEL_EXTRA_FUNCS_H
+
+#endif //HOTEL_EXTRA_FUNCS_H
+
+/**
+ * limpar o terminal
+ */
+void limpar_tela();
+
+/**
+ * limpar buffer do teclado
+ */
+void limpar_teclado();
+
+/**
+ * retorna a string toda em maiuscula
+ * @param str string para fazer uppercase
+ */
+void upper(char *str);
+
+/**
+ * remover quebra de linha de uma string
+ * @param str string para remover a quebra de linha
+ */
+void remover_quebra(char *str);
+
+
+
