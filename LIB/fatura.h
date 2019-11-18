691c635c0a59900574b7766a69356fae2d33d781
diegolima362 <diegolima362@gmail.com>
2019-11-17 22:42:06 -0300

    reiniciando

--- LIB/fatura.h
+++
@@ -1,7 +0,0 @@
-
-
-void criar_fatura(char *fat, double pid);
-void adicionar_valor_fatura(SERVICE srv, int qtd, int alt, char *fat);
-void fechar_fatura(CONTRATO *p);
-void imprimir_fatura(CONTRATO p);
-void nome_fatura(char *str, double id);
